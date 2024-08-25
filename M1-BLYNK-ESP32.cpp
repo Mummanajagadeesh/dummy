8486f3569d67954c66b6cfe7b8ecc3d027b51fe0
M_Jagadeesh97 <75290381+Mummanajagadeesh@users.noreply.github.com>
2024-08-25 17:41:24 +0530

    Update M1-BLYNK-ESP32.cpp

--- M1-BLYNK-ESP32.cpp
+++ M1-BLYNK-ESP32.cpp
@@ -1,27 +1,54 @@
-#include <BlynkSimpleEsp8266.h>
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
 
-char auth[] = "<token>";
-char ssid[] = "<ssid>";
-char pass[] = "<password>";
+const char* ssid = "<SSID>"; 
+const char* password = "<PASSWORD>";
+const char* blynkAuth = "<BLYNK_AUTH_TOKEN>";
 
-int pirPin = D1;  
-int reedPin = D2;  
-bool pirState = false;  
-bool reedState = false;  
+const int pirPins[] = { 23, 22, 21 }; // PIR pins
+const int reedPins[] = { 19, 18 }; // reed switch pins
+
+const int numPIRs = sizeof(pirPins)/sizeof(pirPins[0]); // no of PIRs
+const int numReeds = sizeof(reedPins)/sizeof(reedPins[0]); // no of reed switches
 
 void setup() {
-    Blynk.begin(auth, ssid, pass);
-    pinMode(pirPin, INPUT);
-    pinMode(reedPin, INPUT);
+
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("Connected to WiFi");
+  Blynk.begin(blynkAuth, ssid, password);
+    
+  for (int i = 0; i < numPIRs; i++){
+    pinMode(pirPins[i], INPUT);
+  }
+  for (int i = 0; i < numReeds; i++){
+    pinMode(reedPins[i], INPUT);
+  }
 }
 
-void loop() {
-    pirState = digitalRead(pirPin);
-    reedState = digitalRead(reedPin);
-    if (pirState || reedState) {
-        Blynk.notify("Alert! Motion detected or door/window opened");
+void loop(){
+
+  Blynk.run();
+    
+  for (int i= 0; i< numPIRs; i++){
+    if (digitalRead(pirPins[i]) == HIGH){
+      String message = "Motion detected at PIR sensor " + String(i);
+      Blynk.notify(message);
+      delay(5000);
     }
-    Blynk.run();
-    // Placeholder for future features
+  }
+
+  for (int i = 0; i <numReeds;i++){
+    if (digitalRead(reedPins[i]) == HIGH){
+      String message = "Door/Window " + String(i) + " opened";
+      Blynk.notify(message);
+      delay(5000);
+    }
+  }
+
+  delay(100);
 }
